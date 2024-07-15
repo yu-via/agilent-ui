102bbcbf58bcec3302c67e8741b92f3ee627dbc7
yu-via <yu.sylvia.c@gmail.com>
2024-07-15 13:06:58 -0400

    more graphs and data

--- graphing.py
+++ graphing.py
@@ -1,29 +1,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-def shift_plot_zoom(csv_file):
-    df_full = pd.read_csv(csv_file)
-    df = df_full[df_full['Error'] >=0]
-    grouped = df.groupby('Shift').agg(
-            mean_error=('Error', 'mean'),
-            std_error=('Error', 'std')
-        ).reset_index()
-    plt.figure(figsize=(20, 12))
-    plt.errorbar(grouped['Shift'], grouped['mean_error'], yerr=grouped['std_error'], fmt='o', linestyle='-', color='b', capsize=5)
-    plt.title('Percent Error vs. Shift')
-    plt.xlabel('Shift')
-    plt.ylabel('Percent Error')
-    plt.grid(True)
-    plt.show()
-def shift_plot_full(csv_file):
-    df = pd.read_csv(csv_file)
-    plt.figure(figsize=(20, 12))
-    plt.plot(df['Shift'], df['Error'], marker='o', linestyle='-', color='g')
-    plt.title('Percent Error vs. Shift')
-    plt.xlabel('Shift')
-    plt.ylabel('Percent Error')
-    plt.grid(True)
-    plt.show()
-def freq_dev_plot_zoom(csv_file):
+from itertools import cycle
+
+def freq_dev_error_zoom(csv_file):
     df_full = pd.read_csv(csv_file)
     df = df_full[df_full['Error'] >=0]
     grouped = df.groupby('Freq Dev').agg(
@@ -37,7 +16,7 @@ def freq_dev_plot_zoom(csv_file):
     plt.ylabel('Percent Error')
     plt.grid(True)
     plt.show()
-def freq_dev_plot_full(csv_file):
+def freq_dev_error_full(csv_file):
     df = pd.read_csv(csv_file)
     plt.figure(figsize=(20, 12))
     plt.plot(df['Freq Dev'], df['Error'], marker='o', linestyle='-', color='g')
@@ -46,6 +25,61 @@ def freq_dev_plot_full(csv_file):
     plt.ylabel('Percent Error')
     plt.grid(True)
     plt.show()
-csv_file = 'plots/range50_dev10-880_vd06.csv'
-freq_dev_plot_zoom(csv_file)
-freq_dev_plot_full(csv_file)
+def two_full_plot(csv1,csv2):
+    df1 = pd.read_csv(csv1)
+    df2 = pd.read_csv(csv2)
+    plt.figure(figsize=(20, 12))
+    plt.plot(df1['Freq Dev'], df1['Error'], marker='o', linestyle='-', color='g')
+    plt.plot(df2['Freq Dev'], df2['Error'], marker='o', linestyle='-', color='r')
+    plt.title('Percent Error vs. Freq Dev')
+    plt.xlabel('Freq Dev')
+    plt.ylabel('Percent Error')
+    plt.grid(True)
+    plt.show()
+def combine(file1,file2):
+    df1 = pd.read_csv(file1)
+    df2 = pd.read_csv(file2)
+    
+    combined_df = pd.concat([df1, df2], ignore_index=True)
+    
+    combined_df = combined_df.sort_values(by='Freq Dev')
+    return combined_df
+def combined_plot(df_list,range):
+    plt.figure(figsize=(20, 12))
+    colors = cycle(['b', 'g', 'r', 'c', 'm', 'y', 'k'])
+    for df, color, max_val in zip(df_list, colors, range):
+        df['Normalized'] = df['Freq Dev'] / max_val
+        df = df[df['Error'] <=100]
+        range_value = df['Range'].iloc[0]
+        plt.plot(df['Normalized'], df['Error'], marker='o', linestyle='-', color=color, label=f'Range {range_value}')
+    
+    plt.xscale('log')
+    plt.title('Percent Error vs. Freq Dev')
+    plt.xlabel('Normalized Freq Dev')
+    plt.ylabel('Percent Error')
+    plt.grid(True)
+    plt.legend(title='Range Values')
+    plt.show()
+file100_1 = 'plots/range100_dev0.1-45.csv'
+file100_2 = 'plots/range100_dev50-1800.csv'
+
+file10_1 = 'plots/range10_dev0.1-2.9.csv'
+file10_2 = 'plots/range10_dev5-195.csv'
+
+file50_1 = 'plots/range50_dev0.1-9.5.csv'
+file50_2 = 'plots/range50_dev10-820.csv'
+
+file20_1 = 'plots/range20_dev0.5-4.5.csv'
+file20_2 = 'plots/range20_dev5-365.csv'
+
+file500_1 = 'plots/range500_dev0.05-95.csv'
+file500_2 = 'plots/range500_dev100-9000.csv'
+
+file1000_1 = 'plots/range1000_dev0.1-45.csv'
+file1000_2 = 'plots/range1000_dev50-15000.csv'
+
+file200_1 = 'plots/range200_dev0.05-45.csv'
+file200_2 = 'plots/range200_dev50-3300.csv'
+df_list = [combine(file10_1,file10_2),combine(file20_1,file20_2),combine(file50_1,file50_2),combine(file100_1,file100_2),combine(file200_1,file200_2),combine(file500_1,file500_2),combine(file1000_1,file1000_2)]
+range_list = [158,316,790,1580,3160,7901,15802]
+combined_plot(df_list,range_list)
